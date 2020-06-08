e87068ea0fdd902917ba2e37d19d5e22c2cd1d5d
Harry Hogg <harry@hogg.io>
2020-06-08 21:33:55 +0100

    chore(Firebae): Use auto config snippets

--- site/index.tsx
+++ site/index.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { analytics, initializeApp, performance } from 'firebase/app';
 import Root from './components/Root';
 import URLState from './components/URLState';
-import 'firebase/analytics';
-import 'firebase/performance';
-
-if (process.env.NODE_ENV === 'production' && navigator.userAgent !== 'ReactSnap') {
-  initializeApp({
-    apiKey: 'AIzaSyCdngHXL19yP6mwpCmHs34ByztbHiX7O8A',
-    authDomain: 'hoggio-2524a.firebaseapp.com',
-    databaseURL: 'https://hoggio-2524a.firebaseio.com',
-    projectId: 'hoggio-2524a',
-    storageBucket: 'hoggio-2524a.appspot.com',
-    messagingSenderId: '65615937041',
-    appId: '1:65615937041:web:1025ff2057745fd1217bb0',
-    measurementId: 'G-GW8PTVLF9T',
-  });
-
-  analytics();
-  performance();
-}
 
 const rootElement = document.getElementById('Root');
 
